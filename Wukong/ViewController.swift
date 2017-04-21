d70a082e04b34cfd92f1e5407aba3e448fd3567c
Qusic <hi@qusic.me>
2017-04-21 20:57:06 +0800

    status bar

--- Wukong/ViewController.swift
+++ Wukong/ViewController.swift
@@ -12,21 +12,26 @@ import SafariServices
 
 class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, SFSafariViewControllerDelegate {
 
-    let appURL = URL(string: "https://wukongmusic.us/test")!
+    private let appURL = URL(string: "https://wukongmusic.us/test")!
 
-    lazy var webView: WKWebView = {
+    private lazy var webView: WKWebView = {
         let configuration = WKWebViewConfiguration()
         configuration.mediaTypesRequiringUserActionForPlayback = []
         let view = WKWebView(frame: CGRect.zero, configuration: configuration)
         view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.scrollView.scrollIndicatorInsets = UIEdgeInsets(top: UIApplication.shared.statusBarFrame.size.height, left: 0, bottom: 0, right: 0)
         view.navigationDelegate = self
         view.uiDelegate = self
         return view
     }()
 
+    override var prefersStatusBarHidden: Bool {
+        return false
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(webView)
+        view.insertSubview(webView, at: 0)
         webView.frame = view.bounds
         webView.load(URLRequest(url: appURL))
     }
